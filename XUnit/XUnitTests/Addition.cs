e7c4aa937cf85120e45daead94e767a7056a2a30
Jose.Cruz <Jose.Cruz@highjump.com>
2021-09-06 19:59:17 -0400

    Added HumanResources code, added xUnit tests

--- XUnit/XUnitTests/Addition.cs
+++ XUnit/XUnitTests/Addition.cs
@@ -12,8 +12,9 @@ namespace XUnitTests
         public int RandomNumber()
         {
             var rnd = new Random();
-            return rnd.Next(1, 101);
+            //return rnd.Next(1, 101);
             //return rnd.Next(101, 200);
+            return rnd.Next(600, 700);
         }
 
         public double AddDouble(double num1, double num2)
---
+++ XUnit/XUnitTests/Rest.cs
@@ -0,0 +1,16 @@
+﻿
+namespace XUnitTests
+{
+    public class Rest
+    {
+        public int SubstractInt(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public double SubstractDouble(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
