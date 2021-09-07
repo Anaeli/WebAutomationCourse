1a832bf8484da7542e26f97c8211674489b7b1f6
Daniel-Zubieta <danielz00937@gmail.com>
2021-09-07 16:02:59 -0400

    Additional Change

---
+++ XUnit/XUnitTests/HumanResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitProject
+{
+    public class HumanResources : Worker
+    {
+        public HumanResources(string name, string lastname) : base(name, lastname)
+        {
+
+        }
+    }
+}
