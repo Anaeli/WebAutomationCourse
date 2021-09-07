d2ff5f06fad2c208648219da01af7595bdaab1c8
Daniel <daniel.zubieta@jalasoft.com>
2021-09-07 12:46:47 -0400

    Daniel Zubieta PracticeFirstPart

---
+++ XUnit/XUnitTests/Rest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTests
+{
+    public class Rest
+    {
+        public int RestInt(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public double RestDouble(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public int RandomNumber()
+        {
+            var rnd = new Random();
+            return rnd.Next(600, 701);
+        }
+    }
+}
