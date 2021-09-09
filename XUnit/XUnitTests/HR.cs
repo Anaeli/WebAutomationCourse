1ee3385af710e9487023d5afa14846858d5153f2
Ruddy Torrez Meruvia <Ruddy.Torrez@jalasoft.com>
2021-09-08 22:15:43 -0400

    Task1

---
+++ XUnit/XUnitTests/HR.cs
@@ -0,0 +1,13 @@
+namespace XUnitProject
+{
+    public class HR:Worker
+
+    {
+         public HR (string name, string lastname) : base(name, lastname)
+        { 
+            
+        }
+
+        
+    }
+}
---
+++ XUnit/XUnitTests/Rest.cs
@@ -0,0 +1,21 @@
+namespace XUnitProject
+{
+    public class Rest
+    {
+         public int restInt (int num1, int num2)
+       {
+           int total;
+           total=num1 - num2;
+           return total;
+
+       }
+        public double restDouble (double num1, double num2)
+       {
+           double total;
+           total=num1 - num2;
+           return total;
+
+       }
+        
+    }
+}
