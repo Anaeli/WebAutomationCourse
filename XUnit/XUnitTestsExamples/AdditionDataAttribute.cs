d51827f98f83ac3864265266c2b087cc976e03c6
fernando sivila <fernando.sivila@jalasoft.com>
2021-09-22 23:54:48 -0400

    Practice 2 - Part 2

---
+++ XUnit/XUnitTestsExamples/AdditionDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace XUnitTestsExamples
+{
+    public class AdditionDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { 10, 15, 25 };
+            yield return new object[] { 12, 5, 17 };
+            yield return new object[] { 3, 11, 14 };
+        }
+    }
+}
---
+++ XUnit/XUnitTestsExamples/InternalAdditionData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace XUnitTestsExamples
+{
+    public class InternalAdditionData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { 2, 5, 7 };
+                yield return new object[] { 3, 6, 9 };
+                yield return new object[] { 4, 7, 11 };
+                yield return new object[] { 5, 8, 13 };
+            }
+        }
+    }
+}
