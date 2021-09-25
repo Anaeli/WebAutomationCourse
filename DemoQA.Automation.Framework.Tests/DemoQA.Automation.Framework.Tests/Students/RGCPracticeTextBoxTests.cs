95c71ee58a8027f4a4c0f0d1e83c4655e19b3a85
Reynaldo Guzman <reynaldito@gmail.com>
2021-09-25 18:10:09 -0400

    Selenium - TextBox Task

---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.Tests/Students/RGCPracticeTextBoxTests.cs
@@ -0,0 +1,31 @@
+﻿using DemoQA.Automation.Framework.Wrappers;
+using System;
+using Xunit;
+
+namespace DemoQA.Automation.Framework.Tests
+{
+    public class RGCPracticeTextBoxTests : IDisposable
+    {
+        private RGCPracticeTextBoxWrapper practiceTextBox;
+
+        public RGCPracticeTextBoxTests()
+        {
+            this.practiceTextBox = new RGCPracticeTextBoxWrapper();
+        }
+
+        [Theory]
+        [InlineData("Reynaldo Guzman","rguzman@test.com")]
+        public void validateThatTextBoxIsFilledProperly(string fullName, string eMail)
+        {
+            practiceTextBox.GoToPage();
+            practiceTextBox.FullNameTextBox.SendKeys(fullName);
+            practiceTextBox.EMailTextBox.SendKeys(eMail);
+        }
+    
+        
+        public void Dispose()
+        {
+            practiceTextBox.QuitDriver();
+        }
+    }
+}
