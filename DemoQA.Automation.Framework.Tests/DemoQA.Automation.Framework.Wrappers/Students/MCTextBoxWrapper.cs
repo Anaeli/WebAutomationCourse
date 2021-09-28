8d6dbd5991725404aa5eb741acefa6d8b701557a
Mark-ks <marcomarco0071@gmail.com>
2021-09-28 19:24:50 -0400

    Selenium task 01

---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.Wrappers/Students/MCTextBoxWrapper.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace DemoQA.Automation.Framework.Wrappers
+{
+    class MCTextBoxWrapper
+    {
+        private IWebDriver driver;
+
+        public MCTextBoxWrapper()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public IWebElement FullNameTextBox => driver.FindElement(By.Id("userName"));
+
+        public IWebElement EmailTextBox => driver.FindElement(By.Id("userEmail"));
+
+        public void GoToPage()
+        {
+            string appURL = "https://demoqa.com/text-box";
+            driver.Navigate().GoToUrl(appURL);
+        }
+    }
+}
