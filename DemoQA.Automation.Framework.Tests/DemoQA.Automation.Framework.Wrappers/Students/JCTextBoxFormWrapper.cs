764409e6be51c4668c981769e7be6765c7c9e3a2
Jose.Cruz <Jose.Cruz@highjump.com>
2021-09-28 11:42:05 -0400

    Mapped FullName and Email in TextBox screen

---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.Wrappers/Students/JCTextBoxFormWrapper.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoQA.Automation.Framework.Wrappers
+{
+    public class JCTextBoxFormWrapper
+    {
+        // Web driver constructor
+        private IWebDriver driver;
+        public JCTextBoxFormWrapper()
+        {
+            driver = new ChromeDriver();
+        }
+
+        // Mapping page fields
+        public IWebElement FullNameTextBox => driver.FindElement(By.Id("userName"));
+
+        public IWebElement EmailTextBox => driver.FindElement(By.Id("userEmail"));
+
+        // Navigate to URL
+        public void GoToPage()
+        {
+            string appURL = "https://demoqa.com/text-box";
+            driver.Navigate().GoToUrl(appURL);
+        }
+
+        // Close webdriver instance
+        public void QuitDriver()
+        {
+            try
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                driver?.Close();
+            }
+            catch (WebDriverException e)
+            {
+                Console.WriteLine("Error closing the driver: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Quiting current driver...");
+                driver?.Quit();
+                driver?.Dispose();
+                driver = null;
+            }
+        }
+    }
+}
