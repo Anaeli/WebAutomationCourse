3ee9b114c34940ebf9bc4d5bbe38bc008515648c
Jessica Pena <jessica.pena@highjump.com>
2021-11-04 17:20:47 -0400

    Selenium - TextBox - Jessica Peña

---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.Tests/Students/JMPFPracticeTextBoxTest.cs
@@ -0,0 +1,30 @@
+﻿using DemoQA.Automation.Framework.Wrappers.Students;
+using Xunit;
+
+namespace DemoQA.Automation.Framework.Tests.Students
+{
+    public class JMPFPracticeTextBoxTest
+    {
+
+        private JMPFPracticeTextBoxWrapper jmpfPracticeTextBoxWrapper;
+
+        public JMPFPracticeTextBoxTest()
+        {
+            this.jmpfPracticeTextBoxWrapper = new JMPFPracticeTextBoxWrapper();
+        }
+
+        [Theory]
+        [InlineData("Jessica Pena", "testing@gmail.com")]
+        public void ValidatesThatFormIsFillSuccessfuly(string fullName, string eMail)
+        {
+            jmpfPracticeTextBoxWrapper.GoToPage();
+            jmpfPracticeTextBoxWrapper.FullNameTextBox.SendKeys(fullName);
+            jmpfPracticeTextBoxWrapper.EmailTextBox.SendKeys(eMail);
+        }
+
+        public void Dispose()
+        {
+            jmpfPracticeTextBoxWrapper.QuitDriver();
+        }
+    }
+}
---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.Wrappers/Students/JMPFPracticeTextBoxWrapper.cs
@@ -0,0 +1,35 @@
+﻿using DemoQA.Automation.Framework.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace DemoQA.Automation.Framework.Wrappers.Students
+{
+    public class JMPFPracticeTextBoxWrapper
+    {
+        private IWebDriver driver;
+
+        public JMPFPracticeTextBoxWrapper()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public IWebElement FullNameTextBox => driver.FindElement(By.Id("userName"));
+
+        public IWebElement EmailTextBox => driver.FindElement(By.Id("userEmail"));
+
+        public void GoToPage()
+        {
+            string appURL = "https://demoqa.com/text-box";
+            driver.Navigate().GoToUrl(appURL);
+        }
+
+        public void QuitDriver()
+        {
+            Console.Write("Quiting current driver...");
+            driver.Quit();
+            driver.Dispose();
+            driver = null;
+        }
+    }
+}
