947c8b7b5c9c45831afc0e4caeb3425078083052
Eliana <anelixe_zafiro@hotmail.com>
2021-11-16 21:59:16 -0400

    Class: 11 -16 -2021
    API

---
+++ DemoQA.Automation.Framework.Tests/DemoQA.Automation.Framework.API.Tests/Entities/RelationshipBookUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoQA.Automation.Framework.API.Tests.Entities
+{
+    public class RelationshipBookUser
+    {
+        public string userId { get; set; }
+        public string isbn { get; set; }
+    }
+}
